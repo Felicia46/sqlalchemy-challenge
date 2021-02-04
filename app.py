5a61cd022f0b567cc50c9f04931c14053725a7ec
Felicia46 <70499242+Felicia46@users.noreply.github.com>
2021-02-04 17:17:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,88 @@
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+measurement=Base.classes.measurement
+station=Base.classes.station
+
+session=Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Calculate the date 1 year ago from the last data point in the database
+    one_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    # Perform a query to retrieve the data and precipitation scores
+    prcp_data = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date > one_year_ago).\
+        order_by(measurement.date).all()
+
+
+    # Convert list of tuples into normal list
+    prcp_data = list(np.ravel(prcp_data))
+
+    return jsonify(prcp_data)
+
+
+# @app.route("/api/v1.0/stations")
+# def passengers():
+    
+#     """Return a list of passenger data including the name, age, and sex of each passenger"""
+#     # Query all passengers
+#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+   
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_passengers = []
+#     for name, age, sex in results:
+#         passenger_dict = {}
+#         passenger_dict["name"] = name
+#         passenger_dict["age"] = age
+#         passenger_dict["sex"] = sex
+#         all_passengers.append(passenger_dict)
+
+#     return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
